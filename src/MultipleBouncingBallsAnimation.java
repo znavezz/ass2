bef97cb53c81d99a824deb00ad757911a88b2f32
znavezz <128615830+znavezz@users.noreply.github.com>
2023-04-24 23:42:30 +0300

    v1.7 wrong input bug fixed

--- src/MultipleBouncingBallsAnimation.java
+++ src/MultipleBouncingBallsAnimation.java
@@ -3,37 +3,11 @@
 import biuoop.DrawSurface;
 import biuoop.GUI;
 import biuoop.Sleeper;
-
-
-import java.awt.*;
 import java.util.ArrayList;
+import java.util.Random;
 
-//public class MultipleBouncingBallsAnimation {
-//    static final int WIDTH = 800;
-//    static final int HEIGHT = 600;
-//
-//    public static void main(String[] args) {
-//        GUI gui = new GUI("MultipleBouncingBallsAnimation", WIDTH, HEIGHT);
-//        Sleeper sleeper = new Sleeper();
-//        ArrayList<Ball> balls = new ArrayList<>();
-//        for (String string : args) {
-//            Ball newBall = new Ball(Integer.parseInt(string), 0, 0, WIDTH, HEIGHT);
-//            newBall.setCenter(newBall.generateRandomPoint());
-//            balls.add(newBall);
-//        }
-//        while (true) {
-//            DrawSurface d = gui.getDrawSurface();
-//            for (Ball ball : balls) {
-//                ball.moveOneStep();
-//                ball.drawOn(d);
-////                ball.getInfo();
-//            }
-//            gui.show(d);
-//            sleeper.sleepFor(50);
-//        }
-//    }
-//}
 public class MultipleBouncingBallsAnimation {
+    private static Random rand = new Random();
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
 
@@ -43,8 +17,14 @@ public class MultipleBouncingBallsAnimation {
         ArrayList<Ball> balls = new ArrayList<>();
         Borders borders = new Borders(WIDTH, HEIGHT);
         for (String string : args) {
-            int ballSize = Integer.parseInt(string);
-            balls.add(new Ball(ballSize, borders));
+            try {
+                balls.add(new Ball(Integer.parseInt(string), borders));
+            } catch (Exception e) {
+                balls.add(new Ball((int) rand.nextDouble(Math.min(borders.getRight() - borders.getLeft(),
+                        borders.getDown() - borders.getUp()) / 6) + 10, borders));
+                System.out.println("The input doesn't match the required type (int).");
+                System.out.println("Random point has been generated.");
+            }
         }
         while (true) {
             DrawSurface d = gui.getDrawSurface();
